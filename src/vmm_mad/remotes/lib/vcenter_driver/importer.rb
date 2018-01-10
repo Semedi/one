4fe115bb8d9088d2121d5eabed7efb33da5137ab
semedi <sergio.semedi@gmail.com>
2018-01-10 13:05:00 +0100

    F #1538: import virtual_machines and templates networking update
    F #1538: create_one_network created, it handles cluster selection

--- src/vmm_mad/remotes/lib/vcenter_driver/importer.rb
+++ src/vmm_mad/remotes/lib/vcenter_driver/importer.rb
@@ -801,32 +801,12 @@ def self.import_networks(con_ops, options)
                 ar_str << ",SIZE = \"#{size}\"]"
 
                 n[:one] << ar_str
+                n[:clusters][:one_object] = n[:one]
 
-                one_vn = VCenterDriver::VIHelper.new_one_item(OpenNebula::VirtualNetwork)
-                if n[:clusters] #Distributed Port Group
-                    done = []
-                    for i in 0..n[:clusters][:refs].size-1
-                        cl_id = n[:clusters][:one_ids][i]
-                        next if cl_id == -1 || done.include?(cl_id)
+                one_vn = VCenterDriver::Network.create_one_network(n[:clusters])
 
-                        if done.empty?
-                            rc = one_vn.allocate(n[:one],cl_id.to_i)
-                            one_vn.info
-                        else
-                            one_cluster = VCenterDriver::VIHelper.one_item(OpenNebula::Cluster, cl_id, false)
-                            rc = one_cluster.addvnet(one_vn['ID'].to_i)
-                        end
-                        done << cl_id
-                    end
-                end
-
-                if ::OpenNebula.is_error?(rc)
-                    STDOUT.puts "\n    Error creating virtual network: " +
-                                " #{rc.message}\n"
-                else
-                    STDOUT.puts "\n    OpenNebula virtual network \e[92m#{n[:import_name]}\e[39m " +
-                                "with ID \e[94m#{one_vn.id}\e[39m created with size #{size}!\n"
-                end
+                STDOUT.puts "\n    OpenNebula virtual network \e[92m#{n[:import_name]}\e[39m " +
+                            "with ID \e[94m#{one_vn.id}\e[39m !\n"
             end
         }
     rescue Interrupt => e
